1f3c1b2a8b1cf9a0d7edd26eb3a10fa72be60bf8
nakyoung1 <skrud5730@naver.com>
2025-03-18 12:32:32 +0900

    0

---
+++ src/component/Weather/Weather.jsx
@@ -0,0 +1,10 @@
+function Weather() {
+     return (
+          <div id="weather-container">
+               <div id="weather-icon"></div>
+               <div id="weather">weather</div>
+               <p id="weather-city">2</p>
+          </div>
+     );
+}
+export default Weather;
